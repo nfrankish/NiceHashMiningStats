8b11c74821818113d08b80b3f04edd004e7ed7fc
nfrankish <nathan@frankish.com.au>
2021-03-29 21:18:21 +1000

    Added octune data into the database into oc_devices

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import time
 from decimal import Decimal
 from influxdb import InfluxDBClient
+import octune
 #Seconds for each iteration
 SLEEP_TIME=60
 #Number of interations to go through to get payments
@@ -37,8 +38,50 @@ def getPaymentData(accounts):
 
         except Exception as int:
             print(int)
-    print(data)
     return data
+
+def getOctuneData(octuneAddresses):
+    data = []
+    for address in octuneAddresses:
+        endpointName = address[0]
+        endpoint = address[1]
+        try:
+          private_api = octune.private_api('http://' + endpoint + ':18000')
+          devices_cuda = private_api.request('GET', '/devices_cuda')
+          workers_cuda = private_api.request('GET', '/workers')
+        except Exception as inst:
+            print(inst)
+            continue
+
+        for d in devices_cuda['devices']:
+            for w in workers_cuda['workers']:
+                if d['uuid'] == w['device_uuid']:
+                    continue
+
+            data.append(
+                "oc_devices,endpoint={endpoint},uuid={deviceid},oc_core={oc_core},oc_memory={oc_memory},oc_power_w={oc_power_w},oc_power_tdp={oc_power_tdp}"
+                " kt_avg={kt_avg},kt_min={kt_min},kt_max={kt_max},vram_temperature={vram_temp},gpu_temperature={gpu_temp},temp_warning={temp_warning},hashrate={hashrate}"
+                ",power={power},core={core},memory={memory}".format(
+                    endpoint=endpoint,
+                    deviceid=d['uuid'],
+                    oc_core=d['oc_data']['core_clock_delta'],
+                    oc_memory=d['oc_data']['memory_clock_delta'],
+                    oc_power_w=d['oc_data']['power_limit_watts'],
+                    oc_power_tdp=d['oc_data']['power_limit_tdp'],
+                    vram_temp=d['__gddr6x_temp'],
+                    gpu_temp=d['gpu_temp'],
+                    temp_warning=d['too_hot'],
+                    hashrate=w['algorithms'][0]['speed'],
+                    kt_avg=d['kernel_times']['avg'],
+                    kt_min=d['kernel_times']['min'],
+                    kt_max=d['kernel_times']['max'],
+                    power=d['gpu_power_usage'],
+                    memory=d['gpu_clock_memory'],
+                    core=d['gpu_clock_core']
+
+                ))
+
+        return data
 
 def getRigData(accounts):
     data = []
@@ -99,7 +142,6 @@ def getRigData(accounts):
                     rigLocalProfit=rigLocalProfit
                 ))
 
-    print(data)
     return data
 
 
@@ -118,6 +160,8 @@ def stats():
             loop = 0
 
         loop = loop + 1
+        data = data + getOctuneData(octuneAddresses=secrets.octuneAddresses)
+        print(data)
         try:
             client.write_points(data, time_precision='ms',  protocol='line')
         except Exception as inst:
--- test.py
+++ test.py
@@ -4,44 +4,52 @@ import json
 import time
 from decimal import Decimal
 from influxdb import InfluxDBClient
-
+import octune
 
 
 def stats():
-    client = InfluxDBClient(host=secrets.influxhost)
-    client.switch_database(secrets.influxdb)
+    data =[]
+    endpoint = ""
+    private_api = octune.private_api('http://' + endpoint +':18000')
+    devices_cuda = private_api.request('GET','/devices_cuda')
+    workers_cuda = private_api.request('GET', '/workers')
+    print(workers_cuda)
+    for d in devices_cuda['devices']:
+        for w in workers_cuda['workers']:
+            if d['uuid'] == w['device_uuid']:
+                print("found worker")
+                continue
 
-    data = []
-    for account in secrets.accounts:
-        orgId = account[0]
-        try:
-            private_api = nicehash.private_api('https://api2.nicehash.com', account[0], account[1], account[2])
-            response = private_api.request('GET', '/main/api/v2/mining/rigs/payouts', 'page=&size=100000', '')
-            payments = response['list']
-            for p in payments:
-                paymentid = p["id"]
-                paymenttimestamp = p["created"]
-                paymentamount = float(p["amount"])
-                paymentfee = float(p["feeAmount"])
-                total = paymentamount - paymentfee
-                print("ID {} - Date {} - Amount {:.8f} - Fee {:.8f} - Total {:.8f}".format(paymentid,paymenttimestamp,paymentamount,paymentfee,total))
-                data.append(
-                    "payouts,orgid='{orgId}' paymentid=\"{paymentid}\",paymentamount={paymentamount:.8f},paymentfee={paymentfee:.8f},total={total:.8f}  {timestamp}".format(
-                        orgId=orgId,
-                        paymentid=paymentid,
-                        paymentamount=paymentamount,
-                        paymentfee=paymentfee,
-                        total=total,
-                        timestamp=paymenttimestamp))
+        print(w)
+        data.append(
+            "oc_devices,endpoint={endpoint},uuid={deviceid},oc_core={oc_core},oc_memory={oc_memory},oc_power_w={oc_power_w},oc_power_tdp={oc_power_tdp}"
+            " kt_avg={kt_avg},kt_min={kt_min},kt_max={kt_max},vram_temperature={vram_temp},gpu_temperature={gpu_temp},temp_warning={temp_warning},hashrate={hashrate}"
+            ",power={power},core={core},memory={memory}".format(
+                endpoint=endpoint,
+                deviceid = d['uuid'],
+                oc_core=d['oc_data']['core_clock_delta'],
+                oc_memory=d['oc_data']['memory_clock_delta'],
+                oc_power_w=d['oc_data']['power_limit_watts'],
+                oc_power_tdp=d['oc_data']['power_limit_tdp'],
+                vram_temp=d['__gddr6x_temp'],
+                gpu_temp=d['gpu_temp'],
+                temp_warning=d['too_hot'],
+                hashrate=w['algorithms'][0]['speed'],
+                kt_avg=d['kernel_times']['avg'],
+                kt_min=d['kernel_times']['min'],
+                kt_max=d['kernel_times']['max'],
+                power=d['gpu_power_usage'],
+                memory=d['gpu_clock_memory'],
+                core=d['gpu_clock_core']
 
-        except Exception as int:
-            print(int)
-        print(data)
-    try:
-        client.write_points(data, time_precision='ms', protocol='line')
-    except Exception as inst:
-        print(inst)
+                ))
+    print(data)
+    return data
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    stats()
+    data = stats()
+    client = InfluxDBClient(host=secrets.influxhost)
+    client.switch_database(secrets.influxdb)
+    client.write_points(data, time_precision='ms', protocol='line')
